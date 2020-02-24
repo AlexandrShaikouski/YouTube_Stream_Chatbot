207bf44f3993edebf92a69dd03a5912f5a37a9bc
Aliaksandr_shaykouski <sash_shay@mail.ru>
2020-02-24 16:42:38 +0300

    Added Youtube API Data

---
+++ src/main/java/com/alexshay/youtube/stream/chatbot/connection/Main.java
@@ -0,0 +1,9 @@
+package com.alexshay.youtube.stream.chatbot.connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String videoId = "tmiVvRRT65A";
+        ListLiveChatMessages.action(videoId);
+    }
+}
